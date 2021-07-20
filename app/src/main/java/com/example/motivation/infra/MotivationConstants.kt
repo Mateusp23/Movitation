7a772e24fc723b9f95f6742acc8c85c329c2e50a
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-07-20 11:31:31 -0300

    ajustando menu de escolhas

--- app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -4,6 +4,11 @@ class MotivationConstants private constructor() {
 
     object KEY {
         val PERSON_NAME = "name"
+    }
 
+    object PHRASEFILTER {
+        val ALL = 1
+        val HAPPY = 2
+        val MORNING = 3
     }
 }
