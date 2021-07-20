09a695b7b013cf2023b27bbba93392844baa0c1b
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-07-20 20:27:02 -0300

    gerando frases aleatorias com o Random

--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -6,26 +6,30 @@ import android.view.View
 import com.example.motivation.R
 import com.example.motivation.infra.MotivationConstants
 import com.example.motivation.infra.SecurityPreferences
+import com.example.motivation.repository.Mock
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSecurityPreferences: SecurityPreferences
+    private var mPhraseFilter : Int = MotivationConstants.PHRASEFILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-
         mSecurityPreferences = SecurityPreferences(this)
         textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        //logica inicial de seleção
+        imageAll.setColorFilter(resources.getColor(R.color.purple_700))
+        handleNewPhrase()
 
         buttonNewPhrase.setOnClickListener(this)
         imageAll.setOnClickListener(this)
         imageHappy.setOnClickListener(this)
         imageMorning.setOnClickListener(this)
-
     }
 
     override fun onClick(v: View) {
@@ -49,16 +53,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (id) {
             R.id.imageAll -> {
                 imageAll.setColorFilter(resources.getColor(R.color.purple_700))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.ALL
             }
             R.id.imageHappy -> {
                 imageHappy.setColorFilter(resources.getColor(R.color.purple_700))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
             }
             R.id.imageMorning -> {
                 imageMorning.setColorFilter(resources.getColor(R.color.purple_700))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.MORNING
             }
         }
     }
 
     private fun handleNewPhrase() {
+        textPhrase.text = Mock().getPhrase(mPhraseFilter)
     }
 }
